6b48d504d931209f68a1cb0b364357a3a7a3bdb5
maneike <maneikebmx@icloud.com>
2021-07-02 19:25:00 +0200

    Added navigate function to type Options

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,6 +1,9 @@
 export type Options = {
   options: {
-    navigation: {replace: (arg0: string) => void};
+    navigation: {
+      replace: (arg0: string) => void;
+      navigate: (arg0: string) => void;
+    };
     route: {params: {email: any}};
   };
 };
